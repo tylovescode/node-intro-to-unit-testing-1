4354e0033933eeeb2c6c2b4c3932b5d00302ee7f
Ty Miller <ty.loves.code@gmail.com>
2018-02-10 22:05:49 -0500

    disregard update

--- adder.js
+++ adder.js
@@ -1,5 +1,5 @@
 function adder(a, b) {
-  // validate that both inputs are numbers
+  // Validate that both inputs are numbers
   if (!(typeof a === 'number' && typeof b === 'number')) {
     throw Error('Oh no!');
   }
